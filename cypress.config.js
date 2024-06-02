556473661a1c58113205b83a644b674d37bc02a5
George-Ashraf-Waguih <george.ashraf.waguih@gmail.com>
2024-06-02 20:22:27 +0300

    adding reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,23 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      reportPageTitle: 'Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    browser: 'chrome', // Set the default browser to Chrome
+    
   },
   env: {
     homepage_url: "https://www.emirates.com/eg/english/"
